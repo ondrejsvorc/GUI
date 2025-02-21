23aaba03da0860bc1eb949515692e6d843bbdc85
Ondřej Švorc <52735242+ondrejsvorc@users.noreply.github.com>
2025-02-21 09:51:38 +0100

    Add unimplemented WPF demo

--- TodoListWpf/MainWindow.xaml.cs
+++ TodoListWpf/MainWindow.xaml.cs
@@ -1,23 +1,72 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TodoListWpf;
 
 /// <summary>
-/// Interaction logic for MainWindow.xaml
+/// Interakční logika pro MainWindow.
 /// </summary>
 public partial class MainWindow : Window
 {
+    /// <summary>
+    /// Kolekce úkolů, která automaticky aktualizuje UI při změně.
+    /// </summary>
+    private readonly ObservableCollection<TodoItem> Tasks = [];
+
     public MainWindow()
     {
         InitializeComponent();
+
+        // Nastavení datového zdroje ListBoxu
+        listBoxTasks.ItemsSource = Tasks;
+    }
+
+    /// <summary>
+    /// Přidá nový úkol do kolekce, pokud není text prázdný.
+    /// </summary>
+    private void AddTask_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    /// <summary>
+    /// Aktualizuje vybraný úkol s novým textem z textového pole.
+    /// </summary>
+    private void UpdateTask_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    /// <summary>
+    /// Odstraní vybraný úkol z kolekce.
+    /// </summary>
+    private void DeleteTask_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    /// <summary>
+    /// Při změně výběru v ListBoxu zobrazí vybraný úkol v textovém poli.
+    /// </summary>
+    private void ListBoxTasks_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+
+    }
+
+    /// <summary>
+    /// Uloží úkoly do souboru.
+    /// </summary>
+    private void SaveTasks_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    /// <summary>
+    /// Načtění úkolů ze souboru.
+    /// </summary>
+    private void LoadTasks_Click(object sender, RoutedEventArgs e)
+    {
+
     }
 }
---
+++ TodoListWpf/TodoItem.cs
@@ -0,0 +1,23 @@
+﻿namespace TodoListWpf;
+
+/// <summary>
+/// Třída reprezentující model úkolu.
+/// </summary>
+public class TodoItem
+{
+    /// <summary>
+    /// Název úkolu.
+    /// </summary>
+    public required string Title { get; set; }
+
+    /// <summary>
+    /// Stav úkolu.
+    /// </summary>
+    public bool IsDone { get; set; }
+
+    // Přepsání metody ToString pro zobrazení v ListBoxu
+    public override string ToString()
+    {
+        return Title;
+    }
+}
