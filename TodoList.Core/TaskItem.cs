f391d5e6b5a707d73e2ff0ebb5f1df92423761b2
Ondřej Švorc <52735242+ondrejsvorc@users.noreply.github.com>
2025-03-01 10:09:11 +0100

    Refactor models and service in core project to later generate .dll, translate summary comments to English

--- TodoList.Core/TaskItem.cs
+++ TodoList.Core/TaskItem.cs
@@ -1,15 +1,15 @@
 ﻿namespace TodoList.Core;
 
 /// <summary>
-/// Reprezentuje úkol.
+/// Represents a task.
 /// </summary>
-/// <param name="Id">Identifikátor úkolu.</param>
-/// <param name="Title">Název úkolu.</param>
-/// <param name="Type">Typ úkolu.</param>
-/// <param name="IsDone">Stav úkolu.</param>
+/// <param name="Id">Task identifier.</param>
+/// <param name="Title">Task title.</param>
+/// <param name="Type">Task type.</param>
+/// <param name="IsDone">Task completion status.</param>
 public record TaskItem(Guid Id, string Title, TaskType Type = TaskType.Other, bool IsDone = false);
 
 /// <summary>
-/// Reprezentuje typ úkolu.
+/// Represents the task type.
 /// </summary>
 public enum TaskType { Work = 0, University = 1, Personal = 2, Other = 3 }
